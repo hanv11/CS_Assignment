f7862c912b61d164dc70ff35abcf08790ce6bd17
Ha Nguyen <hanguyen11mm@gmail.com>
2019-04-22 21:01:01 +0700

    Assignment 2

---
+++ Assignmen2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignmen2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
